e897d0a6cb9d75111a055460b3840f04f41de7fd
ManojThamke <you@example.commanojthamke28@hotmail.com>
2025-06-28 16:58:37 +0530

    Initialize React + Tailwind setup

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <button className="px-4 py-2 bg-purple-600 text-white rounded">
+        Tailwind Working!
+      </button>
+    </div>
+  );
+}
+export default App;
